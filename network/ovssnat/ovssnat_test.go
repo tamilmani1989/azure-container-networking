82f386ae86ce7a7ffd00460fe329dd0e07a48cf5
Tamilmani Manoharan <tamanoha@microsoft.com>
2019-08-06 12:15:28 -0700

    fixed UT

--- network/ovssnat/ovssnat_test.go
+++ network/ovssnat/ovssnat_test.go
@@ -3,10 +3,16 @@ package ovssnat
 import (
 	"os"
 	"testing"
+
+	"github.com/Azure/azure-container-networking/netlink"
 )
+
+var anyInterface = "dummy"
 
 func TestMain(m *testing.M) {
 	exitCode := m.Run()
+
+	// Create a dummy test network interface.
 
 	os.Exit(exitCode)
 }
@@ -15,7 +21,16 @@ func TestAllowInboundFromHostToNC(t *testing.T) {
 	client := &OVSSnatClient{
 		snatBridgeIP:          "169.254.0.1/16",
 		localIP:               "169.254.0.4/16",
-		containerSnatVethName: "eth0",
+		containerSnatVethName: anyInterface,
+	}
+
+	if err := netlink.AddLink(&netlink.DummyLink{
+		LinkInfo: netlink.LinkInfo{
+			Type: netlink.LINK_TYPE_DUMMY,
+			Name: anyInterface,
+		},
+	}); err != nil {
+		t.Errorf("Error adding dummy interface %v", err)
 	}
 
 	if err := client.AllowInboundFromHostToNC(); err != nil {
@@ -29,4 +44,6 @@ func TestAllowInboundFromHostToNC(t *testing.T) {
 	if err := client.DeleteInboundFromHostToNC(); err != nil {
 		t.Errorf("Error removing inbound rule: %v", err)
 	}
+
+	netlink.DeleteLink(anyInterface)
 }
