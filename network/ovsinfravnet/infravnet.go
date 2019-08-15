71c20474c2ddd77e6f126b71d8d9fd55d18b38dc
Tamilmani Manoharan <tamanoha@microsoft.com>
2019-08-14 17:41:12 -0700

    addressed review comments

--- network/ovsinfravnet/infravnet.go
+++ network/ovsinfravnet/infravnet.go
@@ -65,11 +65,13 @@ func (client *OVSInfraVnetClient) CreateInfraVnetRules(
 		return err
 	}
 
+	// 0 signifies not to add vlan tag to this traffic
 	if err := ovsctl.AddIpSnatRule(bridgeName, infraIP.IP, 0, infraContainerPort, hostPrimaryMac, hostPort); err != nil {
 		log.Printf("[ovs] AddIpSnatRule failed with error %v", err)
 		return err
 	}
 
+	// 0 signifies not to match traffic based on vlan tag
 	if err := ovsctl.AddMacDnatRule(bridgeName, hostPort, infraIP.IP, client.containerInfraMac, 0, infraContainerPort); err != nil {
 		log.Printf("[ovs] AddMacDnatRule failed with error %v", err)
 		return err
