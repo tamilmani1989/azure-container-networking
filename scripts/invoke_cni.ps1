532f9ba7831f5257ad3ce514fe56933ec37a91d6
Tamilmani Manoharan <tamanoha@microsoft.com>
2019-11-08 18:44:35 -0800

    update script to take dns suffix

--- scripts/invoke_cni.ps1
+++ scripts/invoke_cni.ps1
@@ -1,27 +1,25 @@
 Param(
 	[parameter(Mandatory=$true)]
 	[string] $containerName,
-	
+
 	[parameter(Mandatory=$true)]
 	[string] $namespace,
-	
+
 	[parameter(Mandatory=$true)]
 	[string] $contid,
-	
+
 	[parameter (Mandatory=$true)]
 	[string] $command,
 
 	[parameter (Mandatory=$false)]
-	[string] $dns
+	[string] $dns,
+
+	[parameter (Mandatory=$false)]
+	[string] $dnssuffix
+
 
 )
 
-'''
-usage:
-.\invoke-cni.ps1 <container_name> <namespace> <container_id> [ADD/DEL] <dns_json_array>
-For example,
-.\invoke-cni.ps1 container1 default 051c2709ff8de1a4042d236491e67c9f48ffa1b38d4023cc48675e0f0bd17d8a ADD '["1.2.3.4","5.6.7.8"]'
-'''
 
 $env:CNI_CONTAINERID=$contid
 $env:CNI_COMMAND=$command
@@ -33,6 +31,12 @@ $k8sargs='IgnoreUnknown=1;K8S_POD_NAMESPACE={0};K8S_POD_NAME={1};K8S_POD_INFRA_C
 $env:CNI_ARGS=$k8sargs
 $env:CNI_IFNAME='eth0'
 
+'''
+usage:
+.\invoke-cni.ps1 <container_name> <namespace> <container_id> [ADD/DEL] <dns_array> <dns_suffix>
+<dns_array> - values should be quoted and comma separated
+<dns_suffix> - values should be quoted and comma separated
+'''
 
 $content = Get-Content -Raw -Path C:\k\azurecni\netconf\10-azure.conflist
 $jobj = ConvertFrom-Json $content
@@ -41,16 +45,21 @@ $plugin|add-member -Name "name" -Value $jobj.name -MemberType Noteproperty
 $plugin|add-member -Name "cniVersion" -Value $jobj.cniVersion -MemberType Noteproperty
 $arrayDataType = get-TypeData  System.Array
 Remove-TypeData  System.Array
-if ( $dns -ne "") {
-	$serverarray = convertfrom-json $dns 
-	$serverobj=@{servers=$serverarray}
-	$dnsobj=@{dns=$serverobj}
-	$plugin|add-member -Name "runtimeConfig" -Value $dnsobj -MemberType Noteproperty
 
+
+if ($dns -ne "" -Or $dnssuffix -ne "") {
+	$dnsjson = "[" + $dns + "]"
+	$serverarray = convertfrom-json $dnsjson
+	$dnsobj = New-Object -TypeName PSObject
+	$dnsobj|add-member -Name "servers" -Value $serverarray -MemberType Noteproperty
+	$dnssuffixjson = "[" + $dnssuffix + "]"
+	$searcharray = convertfrom-json $dnssuffixjson
+	$dnsobj|add-member -Name "searches" -Value $searcharray -MemberType Noteproperty
+	$plugin|add-member -Name "runtimeConfig" -Value $dnsobj -MemberType Noteproperty
 }
 
 $jsonconfig=ConvertTo-Json $plugin -Depth 6
 echo $jsonconfig
 $res=(echo $jsonconfig | azure-vnet)
 echo $res
-Update-TypeData -TypeData $arrayDataType 
+Update-TypeData -TypeData $arrayDataType
